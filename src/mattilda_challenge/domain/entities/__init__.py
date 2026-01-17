a400426a9e0f95b92102cbac5bfb2e86183aff92
ae-lexs <ae.aelexs@gmail.com>
2026-01-17 17:19:28 -0600

    feat(domain): Add Student and StudentStatus entities

--- src/mattilda_challenge/domain/entities/__init__.py
+++ src/mattilda_challenge/domain/entities/__init__.py
@@ -1,7 +1,10 @@
 """Domain entities."""
 
 from mattilda_challenge.domain.entities.school import School
+from mattilda_challenge.domain.entities.student import Student, StudentStatus
 
 __all__ = [
     "School",
+    "Student",
+    "StudentStatus",
 ]
